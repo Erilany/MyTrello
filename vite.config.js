b40eed0051e6cea21e0119d5b7e88fdcb20d68ac
Erilany <114103279+Erilany@users.noreply.github.com>
2026-03-12 22:20:07 +0100

    launch.bat

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   base: './',
+  optimizeDeps: {
+    exclude: ['react-redux'],
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
